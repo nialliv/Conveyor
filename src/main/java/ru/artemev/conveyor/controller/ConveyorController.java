a6fff759371be44debfd6235009d1c24b6d706c9
nialliv <artemjevnikita@yandex.ru>
2022-09-05 20:35:57 +0400

    refactor

--- src/main/java/ru/artemev/conveyor/controller/ConveyorController.java
+++ src/main/java/ru/artemev/conveyor/controller/ConveyorController.java
@@ -1,7 +1,6 @@
 package ru.artemev.conveyor.controller;
 
 import io.swagger.annotations.ApiOperation;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
