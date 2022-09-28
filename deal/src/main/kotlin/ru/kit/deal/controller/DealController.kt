aeb6f9452dbb1fb3371ee50f2facbb80b03e82d9
nialliv <artemjevnikita@yandex.ru>
2022-09-28 19:14:36 +0400

    added swagger annotations

--- deal/src/main/kotlin/ru/kit/deal/controller/DealController.kt
+++ deal/src/main/kotlin/ru/kit/deal/controller/DealController.kt
@@ -1,5 +1,7 @@
 package ru.kit.deal.controller
 
+import io.swagger.v3.oas.annotations.Operation
+import io.swagger.v3.oas.annotations.tags.Tag
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.*
 import ru.kit.deal.dto.FinishRegistrationRequestDTO
@@ -8,21 +10,25 @@ import ru.kit.deal.dto.LoanOfferDTO
 import ru.kit.deal.service.DealService
 
 @RestController
-@RequestMapping("deal")
+@RequestMapping("/api")
+@Tag(name = "Сделка")
 class DealController(
     private val dealService: DealService,
 ) {
-    @PostMapping("application")
+    @PostMapping("/deal/application")
+    @Operation(summary = "Расчёт возможных условий кредита")
     fun calculationPossibleLoans(@RequestBody loanApplicationRequestDTO: LoanApplicationRequestDTO): ResponseEntity<List<LoanOfferDTO>> {
         return ResponseEntity.ok(dealService.calculationPossibleLoans(loanApplicationRequestDTO))
     }
 
-    @PutMapping("offer")
+    @PutMapping("/deal/offer")
+    @Operation(summary = "Выбор одного из предложений")
     fun selectOneOfOffers(@RequestBody loanOfferDTO: LoanOfferDTO) {
         dealService.selectOneOfOffers(loanOfferDTO)
     }
 
-    @PutMapping("calculate/{id}")
+    @PutMapping("/deal/calculate/{id}")
+    @Operation(summary = "Завершение регистрации + полный подсчёт кредита")
     fun completionOfRegistration(
         @PathVariable id: Long,
         @RequestBody finishRegistrationRequestDTO: FinishRegistrationRequestDTO
