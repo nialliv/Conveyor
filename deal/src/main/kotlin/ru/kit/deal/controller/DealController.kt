8d971290386350f83de85292482e37557c1756f6
nialliv <artemjevnikita@yandex.ru>
2022-09-28 14:23:04 +0400

    controller added

--- deal/src/main/kotlin/ru/kit/deal/controller/DealController.kt
+++ deal/src/main/kotlin/ru/kit/deal/controller/DealController.kt
@@ -2,10 +2,31 @@ package ru.kit.deal.controller
 
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.*
+import ru.kit.deal.dto.FinishRegistrationRequestDTO
+import ru.kit.deal.dto.LoanApplicationRequestDTO
+import ru.kit.deal.dto.LoanOfferDTO
+import ru.kit.deal.service.DealService
 
 @RestController
 @RequestMapping("deal")
-class DealController {
-    @GetMapping("test")
-    fun test(): ResponseEntity<String> = ResponseEntity.ok("Success");
+class DealController(
+    private val dealService: DealService,
+) {
+    @PostMapping("application")
+    fun calculationPossibleLoans(@RequestBody loanApplicationRequestDTO: LoanApplicationRequestDTO): ResponseEntity<List<LoanOfferDTO>> {
+        return ResponseEntity.ok(dealService.calculationPossibleLoans(loanApplicationRequestDTO))
+    }
+
+    @PutMapping("offer")
+    fun selectOneOfOffers(@RequestBody loanOfferDTO: LoanOfferDTO) {
+        dealService.selectOneOfOffers(loanOfferDTO)
+    }
+
+    @PutMapping("calculate/{id}")
+    fun completionOfRegistration(
+        @PathVariable id: Long,
+        @RequestBody finishRegistrationRequestDTO: FinishRegistrationRequestDTO
+    ) {
+        dealService.completionOfRegistration(id, finishRegistrationRequestDTO)
+    }
 }
