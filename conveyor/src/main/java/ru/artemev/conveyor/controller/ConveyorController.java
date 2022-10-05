ce214989e88d8451322555d5be1fd7c0e8c2e537
nialliv <artemjevnikita@yandex.ru>
2022-10-05 12:33:35 +0400

    added Valid annotation

--- conveyor/src/main/java/ru/artemev/conveyor/controller/ConveyorController.java
+++ conveyor/src/main/java/ru/artemev/conveyor/controller/ConveyorController.java
@@ -3,7 +3,8 @@ package ru.artemev.conveyor.controller;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,28 +13,37 @@ import ru.artemev.conveyor.dto.CreditDTO;
 import ru.artemev.conveyor.dto.LoanApplicationRequestDTO;
 import ru.artemev.conveyor.dto.LoanOfferDTO;
 import ru.artemev.conveyor.dto.ScoringDataDTO;
-import ru.artemev.conveyor.service.IConveyorService;
+import ru.artemev.conveyor.service.ConveyorService;
 
+import javax.validation.Valid;
+import javax.validation.ValidationException;
 import java.util.List;
 
 @RestController
 @RequestMapping("/api")
 @Tag(name = "кредитный конвейер")
+@Validated
 public class ConveyorController {
 
-  @Autowired private IConveyorService conveyorService;
+  @Autowired private ConveyorService conveyorService;
 
   @PostMapping("/conveyor/offers")
   @Operation(summary = "Получение списка LoanOfferDTO")
-  public ResponseEntity<List<LoanOfferDTO>> getOffers(
-      @RequestBody LoanApplicationRequestDTO loanApplicationRequestDTO) {
-    return ResponseEntity.ok(conveyorService.getOffers(loanApplicationRequestDTO));
+  public List<LoanOfferDTO> getOffers(
+      @RequestBody @Valid LoanApplicationRequestDTO loanApplicationRequestDTO,
+      BindingResult bindingResult) {
+    if (bindingResult.hasErrors())
+      throw new ValidationException(bindingResult.getModel().toString());
+    return conveyorService.getOffers(loanApplicationRequestDTO);
   }
 
   @PostMapping("/conveyor/calculation")
   @Operation(
       summary = "Валидация присланных данных + скоринг данных + полный расчет параметров кредита")
-  public ResponseEntity<CreditDTO> getCreditDto(@RequestBody ScoringDataDTO scoringDataDTO) {
-    return ResponseEntity.ok(conveyorService.getCreditDto(scoringDataDTO));
+  public CreditDTO getCreditDto(
+      @RequestBody @Valid ScoringDataDTO scoringDataDTO, BindingResult bindingResult) {
+    if (bindingResult.hasErrors())
+      throw new ValidationException(bindingResult.getModel().toString());
+    return conveyorService.getCreditDto(scoringDataDTO);
   }
 }
