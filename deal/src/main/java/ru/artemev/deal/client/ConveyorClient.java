dda6d8cf9962e542bf2f7a42036bb1d6dfc1dd22
nialliv <artemjevnikita@yandex.ru>
2022-10-05 14:37:07 +0400

    valid and validated annotation added

--- deal/src/main/java/ru/artemev/deal/client/ConveyorClient.java
+++ deal/src/main/java/ru/artemev/deal/client/ConveyorClient.java
@@ -2,6 +2,7 @@ package ru.artemev.deal.client;
 
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import ru.artemev.deal.dto.CreditDTO;
@@ -9,14 +10,16 @@ import ru.artemev.deal.dto.LoanApplicationRequestDTO;
 import ru.artemev.deal.dto.LoanOfferDTO;
 import ru.artemev.deal.dto.ScoringDataDTO;
 
+import javax.validation.Valid;
 import java.util.List;
 
+@Validated
 @FeignClient(name = "conveyor-client", url = "localhost:8080/api")
 public interface ConveyorClient {
-    @PostMapping("/conveyor/offers")
-    ResponseEntity<List<LoanOfferDTO>> getOffers(@RequestBody LoanApplicationRequestDTO loanApplicationRequestDTO);
+  @PostMapping("/conveyor/offers")
+  ResponseEntity<List<LoanOfferDTO>> getOffers(
+      @RequestBody @Valid LoanApplicationRequestDTO loanApplicationRequestDTO);
 
-    @PostMapping("/conveyor/calculation")
-    ResponseEntity<CreditDTO> getCreditDto(@RequestBody ScoringDataDTO scoringDataDTO);
+  @PostMapping("/conveyor/calculation")
+  ResponseEntity<CreditDTO> getCreditDto(@RequestBody @Valid ScoringDataDTO scoringDataDTO);
 }
-
