3b795b7909b19b8b54a76eeef46a774c0687db24
nialliv <artemjevnikita@yandex.ru>
2022-10-05 14:40:54 +0400

    dir with enums and models

--- deal/src/main/java/ru/artemev/deal/model/Passport.java
+++ deal/src/main/java/ru/artemev/deal/model/Passport.java
@@ -1,10 +1,8 @@
-package ru.artemev.deal.dto;
+package ru.artemev.deal.model;
 
 import io.swagger.v3.oas.annotations.media.Schema;
-import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
-import lombok.NoArgsConstructor;
 
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Past;
---
+++ deal/src/main/java/ru/artemev/deal/model/enums/Gender.java
@@ -0,0 +1,14 @@
+package ru.artemev.deal.model.enums;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.AllArgsConstructor;
+
+@Schema(description = "Пол")
+@AllArgsConstructor
+public enum Gender {
+  MALE("Мужчина"),
+  FEMALE("Женщина"),
+  NOT_BINARY("Другое");
+
+  private final String description;
+}
