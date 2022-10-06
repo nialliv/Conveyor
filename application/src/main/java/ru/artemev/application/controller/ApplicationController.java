63f2e20224300dd4ee5b2c490ed8f4ae08568355
nialliv <artemjevnikita@yandex.ru>
2022-10-06 20:51:16 +0400

    Removed ResponseEntity wrapper

--- application/src/main/java/ru/artemev/application/controller/ApplicationController.java
+++ application/src/main/java/ru/artemev/application/controller/ApplicationController.java
@@ -3,31 +3,45 @@ package ru.artemev.application.controller;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 import ru.artemev.application.dto.LoanApplicationRequestDTO;
 import ru.artemev.application.dto.LoanOfferDTO;
 import ru.artemev.application.service.ApplicationService;
 
+import javax.validation.Valid;
+import javax.validation.ValidationException;
 import java.util.List;
 
 @RestController
 @RequestMapping("/api/application")
 @Tag(name = "Заявка")
+@Validated
 public class ApplicationController {
 
   @Autowired private ApplicationService applicationService;
 
   @PostMapping()
   @Operation(summary = "Прескоринг + запрос на расчёт возможных условий кредита.")
-  public ResponseEntity<List<LoanOfferDTO>> getLoanOfferDtoList(
-      @RequestBody LoanApplicationRequestDTO loanApplicationRequestDTO) {
-    return ResponseEntity.ok(applicationService.getLoanOfferDtoList(loanApplicationRequestDTO));
+  public List<LoanOfferDTO> getLoanOfferDtoList(
+      @RequestBody @Valid LoanApplicationRequestDTO loanApplicationRequestDTO,
+      BindingResult bindingResult) {
+    if (bindingResult.hasErrors())
+      throw new ValidationException(bindingResult.getModel().toString());
+    return applicationService.getLoanOfferDtoList(loanApplicationRequestDTO);
   }
 
   @PutMapping("offer")
   @Operation(summary = "Выбор одного из предложений.")
-  public void selectOneOfOffers(@RequestBody LoanOfferDTO loanOfferDTO) {
+  public void selectOneOfOffers(
+      @RequestBody @Valid LoanOfferDTO loanOfferDTO, BindingResult bindingResult) {
+    if (bindingResult.hasErrors())
+      throw new ValidationException(bindingResult.getModel().toString());
     applicationService.selectOneOfOffers(loanOfferDTO);
   }
 }
