cb7173761fd545c60290484a654c316da27e7a45
nialliv <artemjevnikita@yandex.ru>
2022-10-06 20:53:34 +0400

    initialized tests

---
+++ application/src/test/java/ru/artemev/application/service/impl/ApplicationServiceImplTest.java
@@ -0,0 +1,16 @@
+package ru.artemev.application.service.impl;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ApplicationServiceImplTest {
+
+    @Test
+    void getLoanOfferDtoList() {
+    }
+
+    @Test
+    void selectOneOfOffers() {
+    }
+}
