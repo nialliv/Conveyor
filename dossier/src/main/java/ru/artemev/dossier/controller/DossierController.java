8bcca98b40feb5eefe7a8349c4092db307e143eb
nialliv <nartemev@neoflex.ru>
2022-10-11 17:36:19 +0400

    init dossier

---
+++ dossier/src/main/java/ru/artemev/dossier/controller/DossierController.java
@@ -0,0 +1,9 @@
+package ru.artemev.dossier.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/dossier")
+public class DossierController {
+}
---
+++ dossier/src/test/java/ru/artemev/dossier/DossierApplicationTests.java
@@ -0,0 +1,11 @@
+package ru.artemev.dossier;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DossierApplicationTests {
+
+  @Test
+  void contextLoads() {}
+}
