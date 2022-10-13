d7d287e93ce129c343a805338ce6b305084d7984
nialliv <nartemev@neoflex.ru>
2022-10-13 17:18:08 +0400

    added json annotations

--- application/src/main/java/ru/artemev/application/dto/LoanOfferDTO.java
+++ application/src/main/java/ru/artemev/application/dto/LoanOfferDTO.java
@@ -1,5 +1,7 @@
 package ru.artemev.application.dto;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
 import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Builder;
 import lombok.Data;
@@ -9,21 +11,37 @@ import java.math.BigDecimal;
 @Data
 @Builder
 @Schema(description = "Предложение по кредиту")
+@JsonDeserialize(builder = LoanOfferDTO.LoanOfferDTOBuilder.class)
 public class LoanOfferDTO {
   @Schema(description = "Итоговая сумма", example = "15000")
+  @JsonProperty("totalAmount")
   private final BigDecimal totalAmount;
+
   @Schema(description = "Срок займа", example = "10")
+  @JsonProperty("term")
   private final Integer term;
+
   @Schema(description = "Ежемесячный платеж", example = "1500")
+  @JsonProperty("monthlyPayment")
   private final BigDecimal monthlyPayment;
+
   @Schema(description = "Процентная ставка", example = "10")
+  @JsonProperty("rate")
   private final BigDecimal rate;
+
   @Schema(description = "Страховка", example = "true")
+  @JsonProperty("insuranceEnabled")
   private final boolean insuranceEnabled;
+
   @Schema(description = "Зарплатный клиент", example = "true")
+  @JsonProperty("salaryClient")
   private final boolean salaryClient;
+
   @Schema(description = "Номер предложения", example = "4")
+  @JsonProperty("applicationId")
   private Long applicationId;
+
   @Schema(description = "Запрашиваемая сумма", example = "100000")
+  @JsonProperty("requestedAmount")
   private BigDecimal requestedAmount;
 }
