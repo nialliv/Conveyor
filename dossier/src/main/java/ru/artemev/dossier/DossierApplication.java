414a874bfc6169f90bd3c8d7c58aa2fb486bde29
nialliv <nartemev@neoflex.ru>
2022-10-17 18:52:03 +0400

    refactor

--- dossier/src/main/java/ru/artemev/dossier/DossierApplication.java
+++ dossier/src/main/java/ru/artemev/dossier/DossierApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
-@EnableFeignClients
+@EnableFeignClients("ru.artemev.dossier")
 public class DossierApplication {
 
   public static void main(String[] args) {
