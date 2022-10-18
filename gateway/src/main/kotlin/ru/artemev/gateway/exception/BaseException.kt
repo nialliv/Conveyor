4e006dafec49962d0099f7bba688ed24a96f4063
nialliv <nartemev@neoflex.ru>
2022-10-18 12:02:09 +0400

    moved gateway to mvp5 branch

--- gateway/src/main/kotlin/ru/artemev/gateway/exception/BaseException.kt
+++
@@ -1,8 +0,0 @@
-package ru.artemev.gateway.exception
-
-import org.springframework.http.HttpStatus
-
-abstract class BaseException(
-    val httpStatus: HttpStatus,
-    val apiError: ApiError,
-) : RuntimeException(apiError.description)
