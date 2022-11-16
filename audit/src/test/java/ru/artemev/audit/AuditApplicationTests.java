16d042091ef3f19d6a2f2568290462d25c44d9c6
nialliv <nartemev@neoflex.ru>
2022-11-16 13:58:53 +0400

    added audit

---
+++ audit/src/test/java/ru/artemev/audit/AuditApplicationTests.java
@@ -0,0 +1,11 @@
+package ru.artemev.audit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuditApplicationTests {
+
+  @Test
+  void contextLoads() {}
+}
